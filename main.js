412ae19a0eb042962a5596e09d9bf44992a559f0
dengtao <d17381844023@gmail.com>
2025-09-20 12:37:35 +0800

    feat: add file fb2

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 import master from './master.module.js'
 import fa from './fa.js'
 import fb from  './fb.js'
+import fb2 from  './fb2.js'
 
 function main(){
   master()
   fa()
   fb()
+  fb2()
 }
 
 
