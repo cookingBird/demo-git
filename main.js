03698a16aa3d784aea1efc2a431448a1c65a1903
dengtao <d17381844023@gmail.com>
2025-09-21 11:20:07 +0800

    feat: add file fa4

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import master from './master.module.js'
 import fa from './fa.js'
 import fa2 from './fa2.js'
 import fa3 from './fa3.js'
+import fa4 from './fa4.js'
 import fb from  './fb.js'
 import fb2 from  './fb2.js'
 import fb3 from  './fb3.js'
@@ -11,6 +12,7 @@ function main(){
   fa()
   fa2()
   fa3()
+  fa4()
   fb()
   fb2()
   fb3()
